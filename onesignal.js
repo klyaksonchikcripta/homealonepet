7af62bb722407278ba91ee69b71498d25419e20f
klyaksonchikcripta <klyaksonchikcripta@gmail.com>
2025-07-22 02:03:54 +0200

    Update onesignal.js

---
+++ onesignal.js
@@ -0,0 +1,7 @@
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+  OneSignal.init({
+    appId: "6fb0b6cf-9734-41ae-8aec-5675f3e18898",
+    notifyButton: { enable: false }
+  });
+});
